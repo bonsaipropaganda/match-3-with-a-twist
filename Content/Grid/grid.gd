9023ecd18951ec1be97ca829900bb224db130dfe
Coffe <jonathan_davey@outlook.com>
2023-06-19 20:09:58 +1000

    Made prototype work

--- Content/Grid/grid.gd
+++ Content/Grid/grid.gd
@@ -5,92 +5,38 @@ const GRID_COLOR = Color("fce1cf")
 const LINE_WIDTH = 2.0
 const TILE_MARGIN = 3.0
 
-const NO_GROUP = -1
+const TIMESTEP = 0.2
 
-@export var grid_width = 5
-@export var grid_height = 5
+@export var grid_width = 6
+@export var grid_height = 6
 @export var tile_width = 70
-var grid: Array
-var group_counts: Dictionary
 
 @onready var tile_scene := preload("res://Content/Tile/tile.tscn")
 
+var grid: Array
+var group_counts: Dictionary
+var done_updating := false
+
 func _ready():
 	if !Engine.is_editor_hint():
+		$Timer.wait_time = TIMESTEP
+		Globals.connect("swap_tile", on_swap_tile)
 		grid = init_grid(grid_width, grid_height)
-		add_tile(0,0)
-		add_tile(0,1)
-		add_tile(0,3)
-		add_tile(4,2)
-		add_tile(2,0)
+		
+		for y in grid_height:
+			for x in grid_width:
+				add_tile(x,y)
 	queue_redraw()
 
 func _process(delta):
 	if Engine.is_editor_hint():
 		queue_redraw()
 	else:
-		if Input.is_action_just_pressed("ui_accept"):
-			update_grid()
+		if !done_updating && $Timer.is_stopped():
+			$Timer.start()
 
-func _draw():
-	# Vertical lines
-	for x in grid_width + 1:
-		draw_line(Vector2(x * tile_width, 0), Vector2(x * tile_width, grid_height * tile_width), GRID_COLOR, 2.0)
-	# Horizontal lines
-	for y in grid_height + 1:
-		draw_line(Vector2(0, y * tile_width), Vector2(grid_width * tile_width, y * tile_width), GRID_COLOR, 2.0)
-
-func init_grid(width, height, fill_with=null):
-	var g = []
-	for i in height:
-		g.append([])
-		for j in width:
-			g[i].append(fill_with)
-	return g
-
-func set_tile_position(tile, x, y):
-	tile.position = Vector2(x * tile_width, y * tile_width) + Vector2(TILE_MARGIN, TILE_MARGIN)
-
-func add_tile(x, y):
-	if (x < 0 || x >= grid_width || y < 0 || y >= grid_height):
-		assert(false) # Do not try to add tiles out of bounds
-	if (grid[y][x] != null):
-		assert(false) # Do not try to spawn tiles on top of each other
-	
-	var new_tile = tile_scene.instantiate()
-	grid[y][x] = new_tile
-	set_tile_position(new_tile, x, y)
-	new_tile.initialise(tile_width, TILE_MARGIN)
-	add_child(new_tile)
-
-func move_tile(x1, y1, x2, y2):
-	if grid[y1][x1] != null && grid[y2][x2] == null:
-		set_tile_position(grid[y1][x1], x2, y2)
-		grid[y2][x2] = grid[y1][x1]
-		grid[y1][x1] = null
-
-func can_fall(x, y):
-	if y == grid_height - 1:
-		return false
-	else:
-		return grid[y][x] != null && grid[y+1][x] == null
-
-func update_tile_group(x, y, group_id, tile_type):
-	if (x < 0 || x >= grid_width || y < 0 || y >= grid_height || grid[y][x] == null):
-		return
-	if (can_fall(x,y) || grid[y][x].group_id != NO_GROUP || grid[y][x].tile_type != tile_type):
-		return
-	
-	grid[y][x].group_id = group_id
-	if group_counts.get(group_id):
-		group_counts[group_id] += 1
-	else:
-		group_counts[group_id] = 1
-	
-	update_tile_group(x-1, y, group_id, tile_type)
-	update_tile_group(x+1, y, group_id, tile_type)
-	update_tile_group(x, y-1, group_id, tile_type)
-	update_tile_group(x, y+1, group_id, tile_type)
+func _on_timer_timeout():
+	update_grid()
 
 func update_grid():
 	var settled = true
@@ -100,16 +46,145 @@ func update_grid():
 				move_tile(x, y, x, y+1)
 				settled = false
 	
+	var to_pop = []
 	if settled:
-		group_counts = {}
-		var group_id = 0
+		to_pop = get_to_pop()
+		for i in to_pop: # [tile_type, x, y]
+			var x = i[1]
+			var y = i[2]
+			if grid[y][x] != null:
+				grid[y][x].queue_free()
+				grid[y][x] = null
+	
+	if to_pop.size() == 0:
+		done_updating = true
+		for x in grid_width:
+			if (add_tile(x, 0)):
+				done_updating = false
+	else:
+		settled = false
+
+func init_grid(width, height):
+	var g = []
+	for i in height:
+		g.append([])
+		for j in width:
+			g[i].append(null)
+	return g
+
+func _draw():
+	# Vertical lines
+	for x in grid_width + 1:
+		draw_line(Vector2(x * tile_width, 0), Vector2(x * tile_width, grid_height * tile_width), GRID_COLOR, 2.0)
+	# Horizontal lines
+	for y in grid_height + 1:
+		draw_line(Vector2(0, y * tile_width), Vector2(grid_width * tile_width, y * tile_width), GRID_COLOR, 2.0)
+
+func set_tile_scene_position(tile, x, y):
+	tile.position = Vector2(x * tile_width, y * tile_width) + Vector2(TILE_MARGIN, TILE_MARGIN)
+	tile.grid_pos = Vector2(x,y)
+
+func add_tile(x, y) -> bool:
+	if (x < 0 || x >= grid_width || y < 0 || y >= grid_height):
+		assert(false) # Do not try to add tiles out of bounds
+	if (grid[y][x] != null):
+		return false # Do not try to spawn tiles on top of each other
+	
+	var new_tile = tile_scene.instantiate()
+	grid[y][x] = new_tile
+	set_tile_scene_position(new_tile, x, y)
+	new_tile.initialise(tile_width, TILE_MARGIN)
+	add_child(new_tile)
+	return true
+
+# Move tile to empty space
+func move_tile(x1, y1, x2, y2):
+	if grid[y1][x1] != null && grid[y2][x2] == null:
+		set_tile_scene_position(grid[y1][x1], x2, y2)
+		grid[y2][x2] = grid[y1][x1]
+		grid[y1][x1] = null
+
+func can_fall(x, y):
+	if y == grid_height - 1:
+		return false
+	else:
+		return grid[y][x] != null && grid[y+1][x] == null
+
+func get_to_pop():
+	var to_pop := []
+	var to_check := []
+	
+	# Vertical check
+	for x in grid_width:
+		to_check.clear()
 		for y in grid_height:
-			for x in grid_width:
-				if grid[y][x]:
-					grid[y][x].group_id = NO_GROUP
-		for y in grid_height:
-			for x in grid_width:
-				if grid[y][x]:
-					update_tile_group(x, y, group_id, grid[y][x].tile_type)
-				group_id += 1
-		print(group_counts)
+			if grid[y][x]:
+				if to_check.is_empty():
+					to_check.append([grid[y][x].tile_type, x, y])
+				elif grid[y][x].tile_type != to_check.back()[0]:
+					if to_check.size() >= 3:
+						to_pop.append_array(to_check)
+					to_check.clear()
+					to_check.append([grid[y][x].tile_type, x, y])
+				elif grid[y][x].tile_type == to_check.back()[0]:
+					to_check.append([grid[y][x].tile_type, x, y])
+					if y == grid_height-1 && to_check.size() >= 3:
+						to_check.append([grid[y][x].tile_type, x, y])
+						to_pop.append_array(to_check)
+			else:
+				to_check.clear()
+	# Horizontal check
+	for y in grid_height:
+		to_check.clear()
+		for x in grid_width:
+			if grid[y][x]:
+				if to_check.is_empty():
+					to_check.append([grid[y][x].tile_type, x, y])
+				elif grid[y][x].tile_type != to_check.back()[0]:
+					if to_check.size() >= 3:
+						to_pop.append_array(to_check)
+					to_check.clear()
+					to_check.append([grid[y][x].tile_type, x, y])
+				elif grid[y][x].tile_type == to_check.back()[0]:
+					to_check.append([grid[y][x].tile_type, x, y])
+					if x == grid_width-1 && to_check.size() >= 3:
+						to_check.append([grid[y][x].tile_type, x, y])
+						to_pop.append_array(to_check)
+			else:
+				to_check.clear()
+	return to_pop
+
+func on_swap_tile(from_pos, direction):
+	if done_updating:
+		var to_pos = from_pos + direction
+		
+		if (to_pos.x < 0 || to_pos.x >= grid_width || to_pos.y < 0 || to_pos.y >= grid_height):
+			return
+		if (grid[from_pos.y][from_pos.x].tile_type == grid[to_pos.y][to_pos.x].tile_type):
+			return
+		
+		var tmp = grid[from_pos.y][from_pos.x]
+		grid[from_pos.y][from_pos.x] = grid[to_pos.y][to_pos.x]
+		grid[to_pos.y][to_pos.x] = tmp
+		
+		set_tile_scene_position(grid[from_pos.y][from_pos.x], from_pos.x, from_pos.y)
+		set_tile_scene_position(grid[to_pos.y][to_pos.x], to_pos.x, to_pos.y)
+		
+		done_updating = false
+
+#func update_tile_group(x, y, group_id, tile_type):
+#	if (x < 0 || x >= grid_width || y < 0 || y >= grid_height || grid[y][x] == null):
+#		return
+#	if (can_fall(x,y) || grid[y][x].group_id != NO_GROUP || grid[y][x].tile_type != tile_type):
+#		return
+#
+#	grid[y][x].group_id = group_id
+#	if group_counts.get(group_id):
+#		group_counts[group_id] += 1
+#	else:
+#		group_counts[group_id] = 1
+#
+#	update_tile_group(x-1, y, group_id, tile_type)
+#	update_tile_group(x+1, y, group_id, tile_type)
+#	update_tile_group(x, y-1, group_id, tile_type)
+#	update_tile_group(x, y+1, group_id, tile_type)
--- Content/Tile/tile.gd
+++ Content/Tile/tile.gd
@@ -1,9 +1,11 @@
-extends Control
+extends Area2D
 
 const SPRITE_WIDTH = 128.0 # Currently set to width of placeholder image
 
 var tile_type: TileType
 var group_id: int
+var tile_width: int
+var grid_pos: Vector2 # Set by grid
 
 enum TileType {
 	RED,
@@ -30,11 +32,30 @@ const tile_colors = [
 	Color("efe526"),
 ]
 
-func initialise(tile_width : float, margin_width):
-	tile_width -= margin_width * 2
+func initialise(_tile_width : float, margin_width):
+	tile_width = _tile_width - margin_width * 2
 	scale *= tile_width / SPRITE_WIDTH
+	$CollisionShape2D.position = Vector2.ONE * tile_width
+	$CollisionShape2D.shape.size = Vector2.ONE * tile_width * 2
 	
-	randomize()
 	tile_type = randi() % TileType.size()
 	$Sprite2D.texture = tile_images[tile_type]
 	$Sprite2D.modulate = tile_colors[tile_type]
+
+var clicked = false
+func _on_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		clicked = true
+	if Input.is_action_just_released("click"):
+		clicked = false
+
+func _process(delta):
+	if clicked:
+		var mouse_pos = get_viewport().get_mouse_position()
+		var my_pos = $CollisionShape2D.global_position
+		if abs(mouse_pos.x - my_pos.x) > tile_width/2:
+			Globals.emit_signal("swap_tile", grid_pos, Vector2(sign(mouse_pos.x - my_pos.x), 0))
+			clicked = false
+		if abs(mouse_pos.y - my_pos.y) > tile_width/2:
+			Globals.emit_signal("swap_tile", grid_pos, Vector2(0, sign(mouse_pos.y - my_pos.y)))
+			clicked = false
