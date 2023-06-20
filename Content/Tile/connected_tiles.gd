b10efd65719e7778d71231cf0e0f818a7eeec46f
Coffe <jonathan_davey@outlook.com>
2023-06-20 19:45:44 +1000

    Minor cleanup

--- Content/Tile/connected_tiles.gd
+++ Content/Tile/connected_tiles.gd
@@ -5,16 +5,8 @@ var y
 var tile_width
 var TILE_MARGIN
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass#set_tile_scene_position(self,x,y)
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	#self.position = get_global_mouse_position()
-
-func set_tile_scene_position(tile, x, y):
-	tile.position = Vector2(x * tile_width, y * tile_width) + Vector2(TILE_MARGIN, TILE_MARGIN)
-	#tile.grid_pos = Vector2(x,y)
