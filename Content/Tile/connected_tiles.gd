20658be20100be4ed7176524be9dc165b05a414b
Seth Louis <sethvihanlouis@gmail.com>
2023-06-26 11:22:44 -0400

    drag and drop not working
    im gonna watch this tutorial tomorrow and fix it
    
    https://www.youtube.com/watch?v=IaAqhIC5DaI&t=31s&ab_channel=Artindi

--- Content/Tile/connected_tiles.gd
+++ Content/Tile/connected_tiles.gd
@@ -2,22 +2,18 @@ extends Area2D
 class_name ConnectedTiles
 
 var tile_type
-var clicked: bool = false
+var clicked =  false
 var tile_width: int
 
 func _process(delta: float) -> void:
-	drag_pieces()
-
-# TODO: we need to check and see if this scene has been clicked
-# if clicked need to have pos follow the mouse around
+	if clicked:
+		global_position = lerp(global_position,get_global_mouse_position(), 25 * delta)
 
 func _on_tile_clicked():
-	#if clicked == false:
 	clicked = true
-	#else: clicked = false
 
-func drag_pieces():
-	if clicked == true:
-		var mouse_pos = get_global_mouse_position()
-		var target_pos = (mouse_pos - self.position).normalized()
-		self.global_position = $mouse_pos.position
+func _input(event: InputEvent) -> void:
+	if event.is_action_released("click"):
+		print("clicked = ", clicked)
+		clicked = false
+		print("clicked = ", clicked)
