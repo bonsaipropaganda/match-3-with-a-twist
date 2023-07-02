1bc74a72cfa2f41d7190bc4e9f34f0c7266b0a3d
Seth Louis <sethvihanlouis@gmail.com>
2023-07-02 08:57:36 -0400

    added sound effects
    added start button
    settings button
    and match sound effect

--- Content/UserInterface/menus/main_menu.gd
+++ Content/UserInterface/menus/main_menu.gd
@@ -6,10 +6,14 @@ extends Control
 
 
 func _on_settings_button_pressed() -> void:
+	%Click.play()
+	await get_tree().create_timer(.75).timeout
 	menu_holder.open_menu(&"Settings")
 
 
 func _on_start_button_pressed() -> void:
+	%Start.play()
+	await get_tree().create_timer(.75).timeout
 	menu_holder.close_menu()
 	main.start_game()
 
