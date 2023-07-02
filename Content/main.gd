ae4b5b26c1167378a16418067d41ff3aacf1de09
Seth Louis <sethvihanlouis@gmail.com>
2023-07-02 08:37:28 -0400

    added a title screen song
    Added the song Beam by Sinneschlösen. Also I made it so that it switches between songs whether it's on the main menu or during the game.

--- Content/main.gd
+++ Content/main.gd
@@ -10,11 +10,15 @@ var GameScene: PackedScene
 func _ready() -> void:
 	menu_holder.open_menu(&"MainMenu")
 	GameScene = load(game_scene)
+	
 
 
 func start_game() -> void:
 	var scene: Node = GameScene.instantiate()
 	$Game.add_child(scene)
+	%Title.stop()
+	await get_tree().create_timer(1).timeout
+	%Theme.play()
 
 
 func reset_game() -> void:
@@ -25,6 +29,9 @@ func reset_game() -> void:
 func reset_to_main_menu() -> void:
 	_clean_game()
 	menu_holder.open_menu(&"MainMenu")
+	%Theme.stop()
+	await get_tree().create_timer(1).timeout
+	%Title.play()
 
 
 func _clean_game() -> void:
