ca04685ce86fe480605e906cbb64752830651e29
Adam <supermarioenshit@gmail.com>
2023-07-05 15:58:34 +0200

    destroying the ui

--- Content/UserInterface/game_ui.gd
+++ Content/UserInterface/game_ui.gd
@@ -14,7 +14,15 @@ func _on_pause_button_pressed() -> void:
 
 
 func set_moves_left(count: int) -> void:
+	$MarginContainer/Control/MovesLeft/AnimationPlayer.stop()
+	if int(%MovesLeftLabel.text) > count:
+		$MarginContainer/Control/MovesLeft/AnimationPlayer.play("newmove")
+	else:
+		$MarginContainer/Control/MovesLeft/AnimationPlayer.play_backwards("newmove")
+	
 	%MovesLeftLabel.text = "%2d" % [count]
 
 func set_score(count: int) -> void:
 	%ScoreLabel.text = "%2d" % [count]
+	$MarginContainer/Control/Score/AnimationPlayer.stop()
+	$MarginContainer/Control/Score/AnimationPlayer.play("newscore")
